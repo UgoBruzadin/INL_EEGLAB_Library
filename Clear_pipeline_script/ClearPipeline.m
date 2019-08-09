1d9560df0a22ee835be5a62023470987a3e1ae18
Ugo Bruzadin Nunes <ugob@siu.edu>
2019-08-09 12:58:06 -0500

    Update ClearPipeline.m

--- Clear_pipeline_script/ClearPipeline.m
+++ Clear_pipeline_script/ClearPipeline.m
@@ -9,41 +9,35 @@
 clc
 clear
 
-extList={'fdt','set','edf','raw','jpg','.sc','sph','wts'};
+extList={'fdt','set','edf','raw','jpg','.sc','sph','wts','xls','asv','mat'};
 ThisFolder = pwd;
-try mkdir (ThisFolder, '\OLD')
-catch
-end
-DumpFolder = strcat(ThisFolder, '\OLD\');
 FileList = ReadJustNames(ThisFolder, extList);
 FullFileList = ReadFileNames(ThisFolder, extList);
+
 if numel(FileList)~=0
+    
+    t = [datetime('now')];
+    DateString = datestr(t);
+    newOld = strcat('\OLD', DateString);
+    newOld = strrep(newOld,' ','-');
+    newOld = strrep(newOld,':','-');
+
+    try mkdir (ThisFolder, newOld)
+    catch
+    end
+
+    DumpFolder = strcat(ThisFolder, newOld, '\');
+
+   
     save('OriginalListOfFiles.mat','FullFileList'); %saves the table in .mat format
     writecell(FullFileList,'OriginalListOfFiles.xls'); %this line makes the .mat table into .xls; can be changed to any format (.csv, .txt, .dat, etc)  
     for i=1:numel(FileList)
-        DumpedFile = strcat(DumpFolder, FileList{i});
-        if(strcmpi(DumpedFile,FullFileList{i})==0)
-            movefile (FullFileList{i}, DumpedFile);
-        end
+        TF = contains(FullFileList(i).name,'OLD')
+        if TF == 0
+            DumpedFile = strcat(DumpFolder, FileList{i});
+            if(strcmpi(DumpedFile,FullFileList{i})==0)
+                movefile (FullFileList{i}, DumpedFile);
+            end
+        end    
     end    
 end
-
-%This code was made by Ugo Bruzadin Nunes on 2019-04-21
-%Throw this code on the a folder and run
-%It will create an excel table and .mat table from all the data give to the fileMatrix variable
-% 
-% clc
-% clear
-% 
-% fileMatrix = dir('*.dat'); %This Matrix stores all the files, could be any format (change .dat to .txt, .csv, .xls, etc.)
-% 
-% TemporaryTable = {};
-% FinalTable = {};% this is an empty matrix for the final Matrix
-% 
-% for i = 1:length(fileMatrix) %runs this loop /for/ as many .dat filels in the current folder
-%     TemporaryTable = readtable(fileMatrix(i).name);
-%     FinalTable = cat(1,FinalTable,TemporaryTable); % the function cat adds the table from file #(i) to the FinalTable matrix
-% end %ends loop
-% 
-% save('FinalTable.mat','FinalTable'); %saves the table in .mat format
-% writetable(FinalTable,'FinalTable.xls'); %this line makes the .mat table into .xls; can be changed to any format (.csv, .txt, .dat, etc)
